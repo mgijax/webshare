87b6b5c1fecb66d73f21820f1c27bc3c99770295
Peter Frost <Pete.Frost@jax.org>
2011-10-10 19:01:34 +0000

    increasee retry time to one hour

--- 503.php
+++ 503.php
@@ -1,6 +1,6 @@
 <?php
 header('HTTP/1.1 503 Service Temporarily Unavailable',true,503);
 header('Status: 503 Service Temporarily Unavailable');
-header('Retry-After: 600');
+header('Retry-After: 6000');
 print "This page is temporarily unavailable";
 ?>
