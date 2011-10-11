5fc393ec5c17f294969fcd3fb3e4f386a44e02b9
Peter Frost <Pete.Frost@jax.org>
2011-10-11 12:02:39 +0000

    reset change

--- 503.php
+++ 503.php
@@ -1,6 +1,6 @@
 <?php
 header('HTTP/1.1 503 Service Temporarily Unavailable',true,503);
 header('Status: 503 Service Temporarily Unavailable');
-header('Retry-After: 6000');
+header('Retry-After: 3600');
 print "This page is temporarily unavailable";
 ?>
