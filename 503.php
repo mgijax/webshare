c8ff7b027cf9c6aedec2edf07fbd7a6e9a6ff5da
CVS 2 Git Conversion <mgiadmin@jax.org>
2012-02-07 18:25:32 +0000

    This commit was manufactured by cvs2svn to create tag 'Root_tr10269'.
    Sprout from tr10269 2012-02-07 18:25:31 UTC CVS 2 Git Conversion <mgiadmin@jax.org> 'This commit was manufactured by cvs2svn to create branch 'tr10269'.'
    Delete:
        503.php

--- 503.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('HTTP/1.1 503 Service Temporarily Unavailable',true,503);
-header('Status: 503 Service Temporarily Unavailable');
-header('Retry-After: 3600');
-print "This page is temporarily unavailable";
-?>
